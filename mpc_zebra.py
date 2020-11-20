af659b72b97cf526db19a7f70b2168a601069288
Vignesh Rajagopal <vignesh@mpconsultants.org>
2020-11-20 08:17:08 -0600

    enable mpc_zebra to print

--- mpc_zebra.py
+++ mpc_zebra.py
@@ -8,14 +8,14 @@ UID_SHORT_LEN = 5
 UID_DEFAULT_SHORT = UID_DEFAULT_LONG[-UID_SHORT_LEN:]
 
 def print_label(uid_in):
-    label_file = open("label_no_logo.zpl")
+    label_file = open("label_with_logo.zpl")
     zpl_print_str = label_file.read()
     label_file.close()
 
     zpl_print_str = zpl_print_str.replace(UID_DEFAULT_LONG, uid_in)
     zpl_print_str = zpl_print_str.replace(UID_DEFAULT_SHORT, uid_in[-UID_SHORT_LEN:])
     print(zpl_print_str)
-    return
+
     new_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     new_socket.connect((PRINTER_HOST_IP, PRINTER_PORT))
     new_socket.send(zpl_print_str.encode())
