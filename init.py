6ca189082cd0267e4fcf33d3fcfbd868628eb389
Vignesh Rajagopal <vignesh@mpconsultants.org>
2020-11-19 23:10:09 -0600

    add init.py

---
+++ init.py
@@ -0,0 +1,14 @@
+import mpc_zebra
+import mpc_rfid
+
+
+def main():
+    # TODO: implement RFID reader implementation
+    # entered_uid = mpc_rfid.read()
+    # mpc_zebra.print_label(entered_uid)
+
+    # sample test call. WHILE TESTING PLEASE COMMENT THIS LINE
+    mpc_zebra.print_label('E0025E226FC1735W')
+
+if __name__ == '__main__':
+    main()
